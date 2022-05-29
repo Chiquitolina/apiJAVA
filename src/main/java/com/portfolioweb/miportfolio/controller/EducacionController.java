c0aaac73200c74d9c8cf759908115059327d8c5d
Alejandro <aleig.906@gmail.com>
2022-05-29 11:17:01 -0300

    a ver porfa

--- src/main/java/com/portfolioweb/miportfolio/controller/EducacionController.java
+++ src/main/java/com/portfolioweb/miportfolio/controller/EducacionController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author elcap
  */
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins="https://portfolioweb-9e852.web.app")
 @RestController
 public class EducacionController {
     
