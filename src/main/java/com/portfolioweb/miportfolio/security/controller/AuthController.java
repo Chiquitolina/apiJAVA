dce4b190708b8052e972508d22599cd8fe367588
Alejandro <aleig.906@gmail.com>
2022-05-28 21:36:48 -0300

    commit probando deploy

--- src/main/java/com/portfolioweb/miportfolio/security/controller/AuthController.java
+++ src/main/java/com/portfolioweb/miportfolio/security/controller/AuthController.java
@@ -39,7 +39,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins="https://portfolioweb-9e852.web.app")
 public class AuthController {
     
     @Autowired
